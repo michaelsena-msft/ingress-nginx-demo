1a30102ac03b291b4ca8f6ccc587ceb51e7a9c75
Michael Sena <michaelsena@microsoft.com>
2025-10-21 10:50:25 +1100

    Allow multiple patches to the ingress YAML

--- get-ingress-yaml.sh
+++ get-ingress-yaml.sh
@@ -5,5 +5,14 @@ set -eou pipefail
 # Download the Azure ingress-nginx deployment (see: https://kubernetes.github.io/ingress-nginx/deploy/#azure)
 curl -o ingress-nginx.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.3/deploy/static/provider/cloud/deploy.yaml
 
+# To create a patch:
+#cp ingress-nginx.yaml ingress-nginx.yaml.unpatched
+#<make changes>
+#diff -U 5 ingress-nginx.yaml.unpatched ingress-nginx.yaml
+
 # Apply the patch to have an Azure entry point.
-patch -i ./patches/ingress-nginx.add-dns-label.patch ingress-nginx.yaml
+patch -i ./patches/ingress-nginx.add-dns-label.patch ingress-nginx.yaml --no-backup-if-mismatch
+
+if [ "$#" -eq 1 ]; then
+    IMAGE=${1} envsubst < ./patches/ingress-nginx.controller-image.patch | patch ingress-nginx.yaml --no-backup-if-mismatch
+fi
