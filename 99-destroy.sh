9e9b7beb8f8cf5cf9f56ffce1c7355cb1aa6a168
Michael Sena <michaelsena@microsoft.com>
2025-10-21 16:16:30 +1100

    fix RG typo

--- 99-destroy.sh
+++ 99-destroy.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 # Local kubeconfig cleanup (optional)
 k config delete-context "$CLUSTER" || true
 k config delete-cluster "$CLUSTER" || true
-k config unset "users.clusterUser_${RG}_${CLUSTER}" || true
+k config unset "users.clusterUser_${RESOURCE_GROUP}_${CLUSTER}" || true
 
 # Docker ACR clean-up.
 docker logout ${ACR_FQDN}
